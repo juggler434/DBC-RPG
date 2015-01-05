3f09ffc835a373fe1cdcaa0a0a8813615ada7190
Nicholas <nickdowl@gmail.com>
2015-01-04 17:09:31 -0800

    Adds terminal interaction script

--- Unity project/Assets/Scripts/LevelInformation/Props/BasicTerminalInteraction.cs
+++ Unity project/Assets/Scripts/LevelInformation/Props/BasicTerminalInteraction.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class BasicTerminalInteraction : MonoBehaviour {
 
+	// Empty object for player 
+	private GameObject player;
+
+	public bool displayText; 
+	public string = "git "
 	// Use this for initialization
 	void Start () {
 	
