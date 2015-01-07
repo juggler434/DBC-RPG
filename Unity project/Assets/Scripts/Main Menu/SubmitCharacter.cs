d9065a9e0fe7edae14cb1df97ee5b1d08b03ff69
Howard <nickdowl@gmail.com>
2015-01-07 11:05:24 -0800

    Adds Input Button
    - Input button submits name and player max health
    - Input Button sends you to phase 0 (untested)

---
+++ Unity project/Assets/Scripts/Main Menu/SubmitCharacter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SubmitCharacter : MonoBehaviour {
+
+	public InputField name;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private int calcHealth() {
+		return GameInformation.Security * 10;
+		}
+
+	public void sumbit() {
+		GameInformation.MaxHealth = calcHealth();
+		GameInformation.PlayerName = name.text;
+		Application.LoadLevel ("Phase0");
+
+		}
+}
