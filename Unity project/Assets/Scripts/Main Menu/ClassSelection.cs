89f1a81071bf3b95b1af3c150d5a1d8c960110e7
Howard <nickdowl@gmail.com>
2015-01-06 16:31:55 -0800

    Creates New Main Menu
    -Made new Main Menu canvas
    -Made Camera Transition to Chalboard Wall
    -Added Class Selection Buttons
    -Class selection buttons functional

---
+++ Unity project/Assets/Scripts/Main Menu/ClassSelection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChooseFrontEnd(){
+		GameInformation.PlayerClass = new FrontEndClass ();
+	}
+
+	public void ChooseBackEnd(){
+		GameInformation.PlayerClass = new BackEndClass ();
+	}
+
+	public void ChooseFullStack(){
+		GameInformation.PlayerClass = new FullStackClass ();
+	}
+
+}
