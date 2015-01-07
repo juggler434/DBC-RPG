dec83d93621d9c00416cacd0ff9842cd219a711f
Howard <nickdowl@gmail.com>
2015-01-06 17:03:31 -0800

    Adds Stat Tags
    -Added tags for Chalboard stats

--- Unity project/Assets/Scripts/Main Menu/ClassSelection.cs
+++ Unity project/Assets/Scripts/Main Menu/ClassSelection.cs
@@ -25,4 +25,13 @@ public class ClassSelection : MonoBehaviour {
 		GameInformation.PlayerClass = new FullStackClass ();
 	}
 
+	public void SetBaseStats(){
+		GameInformation.Ruby = GameInformation.PlayerClass.Ruby;
+		GameInformation.JavaScript = GameInformation.PlayerClass.JavaScript;
+		GameInformation.SQL = GameInformation.PlayerClass.SQL;
+		GameInformation.KeyboardShortcuts = GameInformation.PlayerClass.KeyboardShortcuts;
+		GameInformation.Security = GameInformation.PlayerClass.Security;
+		
+	}
+
 }
