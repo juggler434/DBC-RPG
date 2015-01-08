334e882b5b95afea960036db973c056e3e2093c6
Angelo Tomasco <angelotomasco@gmail.com>
2015-01-08 15:16:42 -0800

    adds presentation scene and transition to it

---
+++ Unity project/Assets/finalLevelScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finalLevelScript : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 2.0f && Input.GetKeyUp (KeyCode.E)) {
+			AutoFade.LoadLevel("Presentation",1,1,Color.white);
+		}
+	}
+}
